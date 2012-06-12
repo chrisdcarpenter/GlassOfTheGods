510d3f49a25e2818ab8b17d77dab1d2d4fe01ef8
Chritopher D Carpenter <cdcarpenter@gmail.com>
2012-06-11 23:38:32 -0500

    Init Ruby on Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GlassOfTheGods::Application.config.secret_token = '565de2fa554ba5f7ece483a139d31c187b5e89665ca29b08aa0197b2b452d3c617728fbaf6ed27bc6309f4bb625bec878abd4aa92595390d3228121ec77407d3'
